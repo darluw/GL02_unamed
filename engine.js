617930f4a6218981d10b9c70366db6e955d2b9c8
Clement0101 <clementguinet85@gmail.com>
2023-11-13 16:42:08 +0100

    Implémentation de la certification magenta
    Version magenta

--- engine.js
+++ engine.js
@@ -1,7 +1,8 @@
 var Engine = {
     check: function(){
-    console.log("Engine Ready !")
-    return true
+    console.log("Engine Ready !");
+    console.log("Magenta level checked".magenta);
+    return true;
     }
     }
     module.exports = Engine
--- radio.js
+++ radio.js
@@ -1,7 +1,8 @@
 var Radio = {
     check: function(){
-        console.log("Radio Ready !")
-        return true
+        console.log("Radio Ready !");
+        console.log("Magenta level checked".magenta);
+        return true;
     }
     }
     module.exports = Radio
