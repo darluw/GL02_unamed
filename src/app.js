7df52e75c843c439db36ffab3292995b1e48fe65
kinzelinremy <kinzelinr@gmail.com>
2023-11-16 10:35:18 +0100

    ajout des méthode de connexion et d'inscription;

--- src/app.js
+++ src/app.js
@@ -4,44 +4,34 @@ const login = require("./login.js");
 const prompt = require("prompt-sync")();
 
 
-/**let filePath = "../files/U3-p32-Gra-Present_perfect_simple_vs_continuous.gift";
+let app = ()=>{
+    main();
 
-
-
-// Lecture asynchrone (non bloquante)
-fs.readFile(filePath, 'utf8', (err, fileContent) => {
-    if (err) {
-        console.error('Erreur lors de la lecture du fichier :', err);
-        return;
-    }
-    console.log(fileContent);
-
-    // Maintenant, vous pouvez utiliser fileContent comme entrée pour votre analyseur GIFT
-    const quiz = parse.parse(fileContent);
-    console.log(quiz);
-});*/
-
-console.log("Bienvenue dans le quiz");
-console.log("1. Login");
-console.log("2. Register");
-console.log("3. Exit");
-let choice = prompt("Votre choix : ");
-switch (choice){
-    case "1":
-        login.login();
-        break;
-    case "2":
-        login.register();
-        break;
-    case "3":
-        console.log("Bye bye");
-        break;
-    default:
-        console.log("Wrong choice");
-        break;
 }
 
+let main = ()=>{
+    console.log("Bienvenue dans le quiz");
+    console.log("1. Login");
+    console.log("2. Register");
+    console.log("3. Exit");
+    let choice = prompt("Votre choix : ");
+    switch (choice){
+        case "1":
+            login.login();
+            break;
+        case "2":
+            login.register();
+            break;
+        case "3":
+            console.log("Bye bye");
+            break;
+        default:
+            console.log("Wrong choice");
+            break;
+    }
+}
 
+app();
 
 
 
