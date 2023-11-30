efeeba2c0ac6134835e558bcf03028cb98f3ca7d
floporsch911 <flocha002@gmail.com>
2023-11-30 15:39:03 +0100

    Commentaire Acceuil.js

--- src/accueil.js
+++ src/accueil.js
@@ -5,18 +5,26 @@ const app = require("./app.js");
 const vCard = require("./Vcard.js");
 const pathFile = "../utils/users.json";
 
+// Fonction qui affiche le menu d'accueil
 let accueil = (user) => {
+    // Vérifier le type d'utilisateur
     if (user.type === "etudiant") {
+        // Afficher le menu
         console.log("1. Passer un test");
-        console.log("2. Se déconnecter")
+        console.log("2. Se déconnecter");
+        // Demander le choix
         let choice = prompt("Votre Choix : ");
+        // Vérifier le choix
         switch (choice) {
             case "1":
+                // On lance la fonction pour passer un test
                 accueil(user);
                 break;
             case "2":
+                // On se déconnecte
                 break;
             default:
+                // On affiche une erreur et on relance la fonction d'accueil
                 console.log("Wrong choice");
                 accueil(user);
                 break;
@@ -24,26 +32,33 @@ let accueil = (user) => {
 
     }
     else if (user.type === "professeur") {
+        // Afficher le menu
         console.log("1. Créer un QCM");
         console.log("2. Créer une vCard");
         console.log("3. Se déconnecter")
+        // Demander le choix
         let choice = prompt("Votre choix : ");
+        // Vérifier le choix
         switch (choice) {
             case "1":
+                // On lance la fonction pour créer un QCM
                 accueil(user);
                 break;
             case "2":
+                // On lance la fonction pour créer une vCard
                 vCard.createVCard();
                 accueil(user);
                 break;
             case "3": 
+                // On se déconnecte
                 break;
             default:
+                // On affiche une erreur et on relance la fonction d'accueil
                 console.log("Wrong choice");
                 accueil(user);
                 break;
         }
     }
 }
-
+// Export de la fonction pour pouvoir l'appeler dans un autre fichier
 module.exports = {accueil} ; 
