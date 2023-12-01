c75465835dd0c34d79c7576a48a24a18ed1adfd7
Rémy Kinzelin <kinzelinr@gmail.com>
2023-12-01 01:14:57 +0100

    add comment

--- src/testExamen.js
+++ src/testExamen.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const prompt = require("prompt-sync")();
 
-
+/**
+ * Permet de charger ke fichier JSON contenant les questions et les réponses
+ */
 let chargerExamen = () =>{
     let jsonExamen;
 
 
-
+    // Charger le fichier JSON
     fs.readFile("../jsonResult/U2-p22-Gra-Ing_or_inf.gift.json", "utf8", (err, data) => {
 
         if (err) {
@@ -14,6 +16,7 @@ let chargerExamen = () =>{
             return;
         }
         jsonExamen = JSON.parse(data);
+        //commencer le test
         testExamen(jsonExamen);
 
     });
@@ -21,17 +24,21 @@ let chargerExamen = () =>{
 
 }
 
-
+/**
+ * Permet d'éxécuter le test
+ * @param jsonExamen : le fichier JSON contenant les questions et les réponses
+ */
 let testExamen = (jsonExamen) =>{
 
 
     let score = 0;
     let nbQuestions = 0;
+    //Parcourir le fichier JSON
     jsonExamen.forEach(question => {
         const type = question.result[0][0]?.type;
 
 
-
+        //Vérifier le type de la question
         switch (type) {
             case "MC":
                 console.log(question.result[0][0]?.stem.text);
@@ -48,14 +55,19 @@ let testExamen = (jsonExamen) =>{
                 }
                 break;
             case "Matching":
+                // affichage du titre de la question
                 console.log(question.result[0][0]?.title);
                 nbQuestions++;
+                // affichage des questions de gauche
                 let lstQuestions = getQuestionMatching(question.result[0][0]?.matchPairs);
+                // affichage des réponses de droite
                 let lstReponses = getReponsesMatching(question.result[0][0]?.matchPairs);
                 let dictionnaireQuestionReponse = {};
                 let nbreQuestions = lstQuestions.length;
                 for(let i = 0; i < nbreQuestions; i++){
+                    // affichage des questions et des réponses
                     afficherMatchings(lstQuestions, lstReponses);
+                    // récupération des index des questions et des réponses
                     let questionMatching = prompt("veuillez entrer l'index de votre question de gauche : ");
                     while(questionMatching < 0 || questionMatching > lstQuestions.length){
                         questionMatching = prompt("veuillez entrer l'index de votre question de gauche : ");
@@ -64,11 +76,13 @@ let testExamen = (jsonExamen) =>{
                     while(reponseMatching < 0 || reponseMatching > lstReponses.length){
                         reponseMatching = prompt("veuilleez entrer l'index de votre question de droite : ");
                     }
+                    // ajout de la question et de la réponse dans le dictionnaire
                     dictionnaireQuestionReponse[lstQuestions[questionMatching]] = lstReponses[reponseMatching];
+                    // suppression de la question et de la réponse dans les listes
                     lstQuestions[questionMatching] = null;
                     lstReponses[reponseMatching] = null;
                 }
-
+                //calcul du score
                 score += verifierReponseMatching(question.result[0][0]?.matchPairs, dictionnaireQuestionReponse);
                 break;
 
@@ -76,6 +90,11 @@ let testExamen = (jsonExamen) =>{
     });
 }
 
+/**
+ * Permet de créer un tableau contenant les questions d'une question de type Matching
+ * @param matchPairs : la liste des réponses d'une question de matching
+ * @returns {*[]} : le tableau des questions
+ */
 let getQuestionMatching = (matchPairs) =>{
     let lstQuestion = [];
     matchPairs.forEach(matchPair => {
@@ -84,6 +103,11 @@ let getQuestionMatching = (matchPairs) =>{
     return lstQuestion
 }
 
+/**
+ * Permet de créer un tableau contenant les réponses d'une question de type Matching et de les mélanger
+ * @param matchPairs : la liste des réponses d'une question de matching
+ * @returns {*[]} : le tableau des réponses mélangées
+ */
 let getReponsesMatching = (matchPairs) =>{
     let lstReponses = [];
     matchPairs.forEach(matchPair => {
@@ -94,6 +118,11 @@ let getReponsesMatching = (matchPairs) =>{
     return lstReponses
 }
 
+/**
+ * Permet d'afficher les questions au format Matching
+ * @param questions : le tableau des questions
+ * @returns {string} : la chaine de caractère contenant les questions
+ */
 let afficherQuestionsMatching = (questions) =>{
     let res ="";
     for (i = 0; i < questions.length; i++) {
@@ -103,6 +132,11 @@ let afficherQuestionsMatching = (questions) =>{
     return res;
 }
 
+/**
+ * Permet d'afficher les réponses au format Matching
+ * @param reponses : le tableau des réponses
+ * @returns {string} : la chaine de caractère contenant les réponses
+ */
 let afficherReponsesMatching = (reponses) =>{
     let res ="";
     for (i = 0; i < reponses.length; i++) {
@@ -112,6 +146,11 @@ let afficherReponsesMatching = (reponses) =>{
     return res;
 }
 
+/**
+ * Permet d'afficher les questions et les réponses au format Matching
+ * @param lstQuestions : le tableau des questions
+ * @param lstReponses : le tableau des réponses
+ */
 let afficherMatchings = (lstQuestions, lstReponses) =>{
     console.log("Questions : ");
     console.log(afficherQuestionsMatching(lstQuestions));
@@ -119,6 +158,12 @@ let afficherMatchings = (lstQuestions, lstReponses) =>{
     console.log(afficherReponsesMatching(lstReponses));
 }
 
+/**
+ * Permet de vérifier les réponses au format Matching
+ * @param matchPairs : la liste des réponses d'une question de matching
+ * @param dictionnaireQuestionReponse : le dictionnaire contenant les questions et les réponses
+ * @returns {number} : le score
+ */
 let verifierReponseMatching = (matchPairs, dictionnaireQuestionReponse) =>{
     let score = 0;
     matchPairs.forEach(matchPair => {
@@ -130,6 +175,10 @@ let verifierReponseMatching = (matchPairs, dictionnaireQuestionReponse) =>{
     return score;
 }
 
+/**
+ * Permet d'afficher les réponses d'une question de type MC
+ * @param reponses : la liste des réponses
+ */
 let afficherReponses = (reponses) =>{
     for (i = 0; i < reponses.length; i++) {
         console.log(`${i+1} : ${reponses[i].text.text}`);
