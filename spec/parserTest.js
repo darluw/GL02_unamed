049cd536701d09036ae05c9e7fbc470e0e67af4e
kinzelinremy <kinzelinr@gmail.com>
2023-12-05 22:40:36 +0100

    update parserTest.js

--- spec/parserTest.js
+++ spec/parserTest.js
@@ -45,29 +45,29 @@ describe('Tests for JSON parsing and correcting files', function () {
         done();
     });
 
-    it('Should parse files and save JSON results',  function (done) {
+    it('Should parse files and save JSON results',  function () {
         let nbFiles = 0;
         try {
             nbFiles = giftFolder.length;
         } catch (err) {
             console.error("Erreur lors de la lecture du dossier :", err);
-            done.fail(err);
+            throw err; // Throw the error to indicate a test failure
         }
+
         try {
-            parser();
+             parser().then(() => {
+                let nbJsonFiles = 0;
+                try {
+                    nbJsonFiles = jsonFolder.length;
+                } catch (err) {
+                    console.error("Erreur lors de la lecture du dossier :", err);
+                    throw err;
+                }
+                expect(nbFiles).toEqual(nbJsonFiles);
+            });
         } catch (err) {
-
+            console.error("Erreur lors de l'analyse des fichiers :", err);
+            throw err;
         }
-
-        let nbJsonFiles = 0;
-        try {
-            nbJsonFiles = jsonFolder.length;
-        } catch (err) {
-            console.error("Erreur lors de la lecture du dossier :", err);
-            done.fail(err);
-        }
-
-        expect(nbFiles).toEqual(nbJsonFiles);
-        done();
     });
 });
