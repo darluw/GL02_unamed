c4a647ac9f6745b164e37f40f9fd95d9c46ba875
kinzelinremy <kinzelinr@gmail.com>
2023-12-06 11:20:18 +0100

    add updateUser.js

---
+++ src/updateUser.js
@@ -0,0 +1,14 @@
+
+const fs = require('fs');
+
+let updateUser = (user) => {
+    let jsonData = fs.readFileSync("../utils/users.json", "utf8");
+    let jsonContent = JSON.parse(jsonData);
+    let users = jsonContent.users;
+    let index = users.findIndex((u) => u.username === user.username);
+    users[index] = user;
+    jsonContent.users = users;
+    fs.writeFileSync("../utils/users.json", JSON.stringify(jsonContent));
+}
+
+module.exports = { updateUser };
