99623be6658fa9f441bb81c05d11f379388899b6
floporsch911 <flocha002@gmail.com>
2023-12-08 12:55:33 +0100

    ne pas planter l'app quand on se trompe de mdp

--- src/login.js
+++ src/login.js
@@ -2,28 +2,30 @@
 
 const pathFile = "../utils/users.json";
 const fs = require("fs");
-const {accueil} = require("./accueil");
+const { accueil } = require("./accueil");
 const prompt = require("prompt-sync")();
 
 
 
 
-let login = ()=>{
+let login = () => {
     console.log("Login : ");
     let username = prompt("Username : ");
     let password = prompt("Password : ");
     let jsonData = fs.readFileSync(pathFile, "utf8");
     let jsonContent = JSON.parse(jsonData);
     let user = jsonContent.users.find((user) => user.username === username && user.password === password);
-    if(user){
-        console.log("Welcome " + user.username);
-        return user;
-    }else{
+    while (!user) {
         console.log("Wrong username or password");
-        let choice = prompt("Voulez-vous réessayer ou vous inscrire ? (1) Réessayer | (2) s'inscrire | (3) Exit  :")
-        switch (choice){
+        let choice = prompt("Voulez-vous réessayer ou vous inscrire ? (1) Réessayer | (2) S'inscrire | (3) Exit  :")
+        switch (choice) {
             case "1":
-                login();
+                console.log("Login : ");
+                let username = prompt("Username : ");
+                let password = prompt("Password : ");
+                let jsonData = fs.readFileSync(pathFile, "utf8");
+                let jsonContent = JSON.parse(jsonData);
+                user = jsonContent.users.find((user) => user.username === username && user.password === password);
                 break;
             case "2":
                 register();
@@ -36,18 +38,20 @@ let login = ()=>{
                 break;
         }
     }
+    console.log("Welcome " + user.username);
+    return user;
 }
 
-let register = ()=>{
+let register = () => {
     console.log("Register : ");
     let username = prompt("Username : ");
     let password = prompt("Password : ");
-    while(password.length < 8){
+    while (password.length < 8) {
         console.log("Password too short");
         password = prompt("Password(min: 8 char) : ");
     }
     let type = prompt("Type(etudiant|professeur) : ");
-    while(type !== "etudiant" && type !== "professeur"){
+    while (type !== "etudiant" && type !== "professeur") {
         console.log("Wrong type");
         type = prompt("Type(etudiant|professeur) : ");
     }
@@ -55,11 +59,11 @@ let register = ()=>{
     let jsonContent = JSON.parse(jsonData);
     const users = jsonContent.users;
     let user = users.find((user) => user.username === username);
-    if(user){
+    if (user) {
         console.log("Username already taken");
         register();
-    }else{
-        jsonContent.users.push({username: username, password: password,type: type, tests: []});
+    } else {
+        jsonContent.users.push({ username: username, password: password, type: type, tests: [] });
         fs.writeFileSync(pathFile, JSON.stringify(jsonContent));
         console.log("User created");
         let user = login();
@@ -68,5 +72,5 @@ let register = ()=>{
 }
 
 
-module.exports = {login, register};
+module.exports = { login, register };
 
